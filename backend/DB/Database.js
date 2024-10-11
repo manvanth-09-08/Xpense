443b18f95c15b3a0ebe0f70cec5fc4001064dec5
manvanth-09-08 <manvanthbs47@gmail.com>
2024-10-11 10:20:46 +0530

    backend

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
-    const db = process.env.MONGO_URL;
+    try{
+        const db = process.env.MONGO_URL;
 
     const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
 
     console.log(`MongoDB Connected to ${connection.host}`);
+    }catch(err){
+        console.log(err)
+    }
+    
 
 }
