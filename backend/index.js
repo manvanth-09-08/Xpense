9820cc49878beac9c233926eb5faaec3950c5d21
manvanth-09-08 <manvanthbs47@gmail.com>
2024-10-11 10:42:19 +0530

    cors3

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,15 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");  // '*' allows all origins
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
