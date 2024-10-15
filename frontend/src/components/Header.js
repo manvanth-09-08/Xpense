f3406ad5b50a8bb93642b8a38c8707a12010afc6
manvanth-09-08 <manvanthbs47@gmail.com>
2024-10-15 09:52:50 +0530

    header responsive

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,159 +1,329 @@
-// NavbarComponent.js
-import React, { useCallback, useEffect, useState } from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
-import "./style.css";
-import { useNavigate } from 'react-router-dom';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-const Header = ({setBankShow, setCategoryShow}) => {
+// // NavbarComponent.js
+// import React, { useCallback, useEffect, useState } from 'react';
+// import { Navbar, Nav, Button } from 'react-bootstrap';
+// import "./style.css";
+// import { useNavigate } from 'react-router-dom';
+// import Particles from "react-tsparticles";
+// import { loadFull } from "tsparticles";
+// const Header = ({setBankShow, setCategoryShow}) => {
   
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
-  const handleShowLogin = () =>{
-    navigate("/login");
-  }
+//   const handleShowLogin = () =>{
+//     navigate("/login");
+//   }
 
+//   const [user, setUser] = useState();
+
+//   useEffect(() => {
+    
+//       if (localStorage.getItem("user")) {
+//         const user = JSON.parse(localStorage.getItem("user"));
+        
+//         setUser(user);
+        
+//       }
+
+
+    
+//   }, []);
+
+//   const handleShowLogout = () => {
+//     localStorage.removeItem("user");
+//     navigate("/login");
+//   }
+
+//   const particlesInit = useCallback(async (engine) => {
+//     // console.log(engine);
+//     await loadFull(engine);
+//   }, []);
+
+//   const particlesLoaded = useCallback(async (container) => {
+//     // await console.log(container);
+//   }, []);
+  
+//   return (
+//     <>
+//     <div style={{ position: 'relative', overflow: 'hidden' }}>
+//       <Particles
+//         id="tsparticles"
+//         init={particlesInit}
+//         loaded={particlesLoaded}
+//         options={{
+//           background: {
+//             color: {
+//               value: '#000',
+//             },
+//           },
+//           fpsLimit: 60,
+//           particles: {
+//             number: {
+//               value: 200,
+//               density: {
+//                 enable: true,
+//                 value_area: 800,
+//               },
+//             },
+//             color: {
+//               value: '#ffcc00',
+//             },
+//             shape: {
+//               type: 'circle',
+//             },
+//             opacity: {
+//               value: 0.5,
+//               random: true,
+//             },
+//             size: {
+//               value: 3,
+//               random: { enable: true, minimumValue: 1 },
+//             },
+//             links: {
+//               enable: false,
+//             },
+//             move: {
+//               enable: true,
+//               speed: 2,
+//             },
+//             life: {
+//               duration: {
+//                 sync: false,
+//                 value: 3,
+//               },
+//               count: 0,
+//               delay: {
+//                 random: {
+//                   enable: true,
+//                   minimumValue: 0.5,
+//                 },
+//                 value: 1,
+//               },
+//             },
+//           },
+//           detectRetina: true,
+//         }}
+//         style={{
+//           position: 'absolute',
+//           zIndex: -1,
+//           top: 0,
+//           left: 0,
+//           right: 0,
+//           bottom: 0,
+//         }}
+//       />
+//     <Navbar className="navbarCSS" collapseOnSelect expand="lg" style={{position: 'relative', zIndex: "2 !important"}}>
+//       {/* <Navbar className="navbarCSS" collapseOnSelect expand="lg" bg="dark" variant="dark"> */}
+//         <Navbar.Brand href="/" className="text-white navTitle">XPENS</Navbar.Brand>
+//         <Navbar.Toggle
+//             aria-controls="basic-navbar-nav"
+//             style={{
+//               backgroundColor: "transparent",
+//               borderColor: "transparent",
+//             }}
+//           >
+//             <span
+//               className="navbar-toggler-icon"
+//               style={{
+//                 background: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(255, 255, 255)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`,
+//               }}
+//             ></span>
+//           </Navbar.Toggle>
+//         <div>
+//         <Navbar.Collapse id="responsive-navbar-nav" style={{color: "white"}}>
+//           {user ? (
+//             <>
+//             <Nav>
+//               <Button variant="outline-light" onClick={()=>setBankShow(true)}>Banks</Button>
+//             </Nav>
+//             <Nav>
+//               <Button variant="outline-light" onClick={()=>setCategoryShow(true)}>Categoies</Button>
+//             </Nav>
+//             <Nav>
+//                 <Button variant="danger" color="error" onClick={handleShowLogout} className="ml-2">Logout</Button>
+//               </Nav>
+//             </>
+//           ) : (
+
+//             <>
+//               <Nav>
+//                 <Button variant="primary" onClick={handleShowLogin} className="ml-2">Login</Button>
+//               </Nav>
+//             </>
+//           )}
+          
+//         </Navbar.Collapse>
+//       </div>
+//       </Navbar>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Header;
+
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import './style.css';
+import { useNavigate } from 'react-router-dom';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
+const Header = ({ setBankShow, setCategoryShow }) => {
+  const navigate = useNavigate();
   const [user, setUser] = useState();
 
+  const handleShowLogin = () => {
+    navigate('/login');
+  };
+
   useEffect(() => {
-    
-      if (localStorage.getItem("user")) {
-        const user = JSON.parse(localStorage.getItem("user"));
-        
-        setUser(user);
-        
-      }
-
-
-    
+    if (localStorage.getItem('user')) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      setUser(user);
+    }
   }, []);
 
   const handleShowLogout = () => {
-    localStorage.removeItem("user");
-    navigate("/login");
-  }
+    localStorage.removeItem('user');
+    navigate('/login');
+  };
 
   const particlesInit = useCallback(async (engine) => {
-    // console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
-    // await console.log(container);
+    // particles loaded
   }, []);
-  
+
   return (
     <>
-    <div style={{ position: 'relative', overflow: 'hidden' }}>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: '#000',
-            },
-          },
-          fpsLimit: 60,
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: true,
-                value_area: 800,
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: '#000',
               },
             },
-            color: {
-              value: '#ffcc00',
-            },
-            shape: {
-              type: 'circle',
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-            },
-            size: {
-              value: 3,
-              random: { enable: true, minimumValue: 1 },
-            },
-            links: {
-              enable: false,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-            },
-            life: {
-              duration: {
-                sync: false,
-                value: 3,
-              },
-              count: 0,
-              delay: {
-                random: {
+            fpsLimit: 60,
+            particles: {
+              number: {
+                value: 200,
+                density: {
                   enable: true,
-                  minimumValue: 0.5,
+                  value_area: 800,
                 },
-                value: 1,
+              },
+              color: {
+                value: '#ffcc00',
+              },
+              shape: {
+                type: 'circle',
+              },
+              opacity: {
+                value: 0.5,
+                random: true,
+              },
+              size: {
+                value: 3,
+                random: { enable: true, minimumValue: 1 },
+              },
+              links: {
+                enable: false,
+              },
+              move: {
+                enable: true,
+                speed: 2,
+              },
+              life: {
+                duration: {
+                  sync: false,
+                  value: 3,
+                },
+                count: 0,
+                delay: {
+                  random: {
+                    enable: true,
+                    minimumValue: 0.5,
+                  },
+                  value: 1,
+                },
               },
             },
-          },
-          detectRetina: true,
-        }}
-        style={{
-          position: 'absolute',
-          zIndex: -1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}
-      />
-    <Navbar className="navbarCSS" collapseOnSelect expand="lg" style={{position: 'relative', zIndex: "2 !important"}}>
-      {/* <Navbar className="navbarCSS" collapseOnSelect expand="lg" bg="dark" variant="dark"> */}
-        <Navbar.Brand href="/" className="text-white navTitle">XPENS</Navbar.Brand>
-        <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            style={{
-              backgroundColor: "transparent",
-              borderColor: "transparent",
-            }}
-          >
-            <span
-              className="navbar-toggler-icon"
-              style={{
-                background: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(255, 255, 255)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`,
-              }}
-            ></span>
-          </Navbar.Toggle>
-        <div>
-        <Navbar.Collapse id="responsive-navbar-nav" style={{color: "white"}}>
-          {user ? (
-            <>
-            <Nav>
-              <Button variant="outline-light" onClick={()=>setBankShow(true)}>Banks</Button>
-            </Nav>
-            <Nav>
-              <Button variant="outline-light" onClick={()=>setCategoryShow(true)}>Categoies</Button>
-            </Nav>
-            <Nav>
-                <Button variant="danger" color="error" onClick={handleShowLogout} className="ml-2">Logout</Button>
-              </Nav>
-            </>
-          ) : (
+            detectRetina: true,
+          }}
+          style={{
+            position: 'absolute',
+            zIndex: -1,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        />
+        <Navbar
+          className="navbarCSS"
+          collapseOnSelect
+          expand="lg"
+          style={{ position: 'relative', zIndex: '2' }}
+        >
+          <Container>
+            <Navbar.Brand href="/" className="text-white navTitle">
+              XPENS
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{ borderColor: 'white'}}> <span className="navbar-toggler-icon"></span></Navbar.Toggle>
 
-            <>
-              <Nav>
-                <Button variant="primary" onClick={handleShowLogin} className="ml-2">Login</Button>
-              </Nav>
-            </>
-          )}
-          
-        </Navbar.Collapse>
-      </div>
-      </Navbar>
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+  {user ? (
+    <>
+      <Nav>
+        <Button
+          variant="outline-light"
+          onClick={() => setBankShow(true)}
+          className="mx-1 navButton"
+        >
+          Banks
+        </Button>
+      </Nav>
+      <Nav>
+        <Button
+          variant="outline-light"
+          onClick={() => setCategoryShow(true)}
+          className="mx-1 navButton"
+        >
+          Categories
+        </Button>
+      </Nav>
+      <Nav>
+        <Button
+          variant="danger"
+          onClick={handleShowLogout}
+          className="mx-1 navButton"
+        >
+          Logout
+        </Button>
+      </Nav>
+    </>
+  ) : (
+    <Nav>
+      <Button
+        variant="primary"
+        onClick={handleShowLogin}
+        className="mx-1 navButton"
+      >
+        Login
+      </Button>
+    </Nav>
+  )}
+</Navbar.Collapse>
+
+          </Container>
+        </Navbar>
       </div>
     </>
   );
